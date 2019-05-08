a90356703a13777851d337a1cfe82df18c8c91f3
saltchang <kk3684635@gmail.com>
2019-05-08 23:05:47 +0800

    making more more more function...

---
+++ dcthr/dcthr.go
@@ -0,0 +1,145 @@
+package dcthr
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"math"
+	"net/http"
+)
+
+var (
+	accessToken = "ivUzHzaPKbGeVpnPg8zMxTAn6lVXrzZgBf3HqV0xpUS6yrKB2Ff4zD5s2nLfoaRq"
+	bearingLib  = [8]float64{0, 45, 90, 135, 180, 225, 270, 315}
+	const3to15  = [3]float64{3, 15, 61}
+	const10to50 = [3]float64{10, 50, 41}
+)
+
+// Path struct
+type Path struct {
+	Start struct {
+		Lat float64
+		Lng float64
+	}
+	End struct {
+		Lat float64
+		Lng float64
+	}
+}
+
+// Point struct
+type Point struct {
+	Elevation  float64   `json:"elevation"`
+	Location   *Location `json:"location"`
+	Resolution float64   `json:"resolution"`
+}
+
+// Location struct
+type Location struct {
+	Lat float64 `json:"lat"`
+	Lng float64 `json:"lng"`
+}
+
+// GetHeightData func
+func GetHeightData(RFLat, RFLng float64) ([8][]float64, [8][]float64) {
+	var path3to15 [8]Path
+	var path10to50 [8]Path
+
+	for index, bearing := range bearingLib {
+		path3to15[index].Start.Lat, path3to15[index].Start.Lng = getLocation(RFLat, RFLng, bearing, 3)
+		path3to15[index].End.Lat, path3to15[index].End.Lng = getLocation(RFLat, RFLng, bearing, 15)
+		path10to50[index].Start.Lat, path10to50[index].Start.Lng = getLocation(RFLat, RFLng, bearing, 10)
+		path10to50[index].End.Lat, path10to50[index].End.Lng = getLocation(RFLat, RFLng, bearing, 50)
+	}
+
+	var data3to15 [8][]float64
+	var data10to50 [8][]float64
+
+	for i := 0; i < 8; i++ {
+		point3to15start := [2]float64{path3to15[i].Start.Lat, path3to15[i].Start.Lng}
+		point3to15end := [2]float64{path3to15[i].End.Lat, path3to15[i].End.Lng}
+		data3to15[i] = getElevation(point3to15start, point3to15end, 61)
+
+		point10to50start := [2]float64{path10to50[i].Start.Lat, path10to50[i].Start.Lng}
+		point10to50end := [2]float64{path10to50[i].End.Lat, path10to50[i].End.Lng}
+		data10to50[i] = getElevation(point10to50start, point10to50end, 41)
+
+		fmt.Printf("第 %d 筆高度資料獲取完成，共 8 筆資料...\n", i+1)
+	}
+
+	for _, data := range data3to15 {
+		fmt.Println(data)
+	}
+	for _, data := range data10to50 {
+		fmt.Println(data)
+	}
+
+	return data3to15, data10to50
+}
+
+// Get elevations by api from JawgMaps
+// https://www.jawg.io/docs/apidocs/elevation/#examples
+func getElevation(pStart, pEnd [2]float64, samples int) []float64 {
+
+	s := "%7C"
+
+	url := fmt.Sprintf("https://api.jawg.io/elevations?path=%f,%f%s%f,%f&samples=%d&access-token=%s", pStart[0], pStart[1], s, pEnd[0], pEnd[1], samples, accessToken)
+
+	res, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer res.Body.Close()
+	// sitemap, err := ioutil.ReadAll(res.Body)
+	var pointList []Point
+	err = json.NewDecoder(res.Body).Decode(&pointList)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var result []float64
+	for _, point := range pointList {
+		point.Elevation = math.Round(point.Elevation)
+		result = append(result, point.Elevation)
+	}
+
+	return result
+
+	// fmt.Printf("%v", pointList)
+}
+
+// Get the target location by given an initial position, distance and the bearing angle.
+// It returns two float64 type latitude and longitude as output.
+// 	Input arguments:
+// 		- latIn: Latitude, in decimal
+// 		- lngIn: Longitude, in decimal
+// 		- theta: Bearing, in degree
+// 		- distance: Distance, in km
+func getLocation(latIn, lngIn, theta, distance float64) (float64, float64) {
+
+	var (
+		R = 6371.0 * 1000.0 // m
+
+		lngOut float64
+		latOut float64
+	)
+
+	distance = distance * 1000.0
+
+	theta = theta * (math.Pi / 180.0)
+
+	latIn = latIn * (math.Pi / 180.0)
+	lngIn = lngIn * (math.Pi / 180.0)
+
+	latOut = math.Asin(math.Sin(latIn)*math.Cos(distance/R) + math.Cos(latIn)*math.Cos(theta)*math.Sin(distance/R))
+
+	lngOut = lngIn + math.Atan((math.Cos(latIn)*math.Sin(theta)*math.Sin(distance/R))/(math.Cos(distance/R)-math.Sin(latIn)*math.Sin(latOut)))
+
+	latOut = latOut * (180.0 / math.Pi) * 1.000002
+	lngOut = lngOut * (180.0 / math.Pi) * 1.000002
+
+	fmt.Printf("Output location: [%.7f, %.7f]\n", latOut, lngOut)
+
+	return latOut, lngOut
+
+}
---
+++ rfcollec/rfcollec.go
@@ -0,0 +1,119 @@
+package rfcollec
+
+import "fmt"
+
+// RFDATA struct
+type RFDATA struct {
+	Basic struct {
+		// 電台名稱 [B3]
+		Name string
+
+		// 射頻類型 (FM or AM)
+		RFTYPE string
+
+		// 電台頻率 (FM: MHz, AM: KHz) [B4]
+		Fequency float64
+
+		// 發射站座標 (緯度, 經度)
+		RFLocation [2]float64
+	}
+
+	Height struct {
+		// 地形取樣點數量
+		N3to15  int
+		N10to50 int
+
+		// 所有取樣點高度資料 (m)
+		H3to15  [8][]float64
+		H10to50 [8][]float64
+
+		// 平均地形海拔高度 (m) [B12:I12]
+		Hav3to15  [8]float64
+		Hav10to50 [8]float64
+
+		// 總平均地形海拔高度 (m)
+		Htoav3to15  float64
+		Htoav10to50 float64
+
+		// 地形起伏度 (dm) [B18:I18]
+		DeltaH [8]float64
+
+		// 地形起伏校正因數 (dB) [B19:I19]
+		DeltaF [8]float64
+	}
+
+	Antenna struct {
+		// 天線增益 (KW, dBd) [B6, D6]
+		Ga [2]float64
+
+		// 天線輻射中心海拔高度 (m) [B11]
+		Hr int
+
+		// 有效天線高度 (m) [B13:I13]
+		HAAT [8]float64
+
+		// 天線相對場型比 [B14:I14]
+		AFR [8]float64
+
+		// 天線相對場型比 (dB) [B15:I15]
+		AFRdB [8]float64
+
+		// 各方位天線增益 (dB) [B16:I16]
+		GadB [8]float64
+	}
+
+	Lost struct {
+		// 傳輸線損失 (dB) [B7]
+		Lt1c float64
+
+		// 接頭損失 (dB) [B8]
+		Lt2t float64
+	}
+
+	Power struct {
+		// 預估發射功率 (KW, dBK) [B5, D5]
+		P0 [2]float64
+
+		// 發射機輸出端有效輻射功率 (dBK) [B9]
+		Pt float64
+
+		// 各方位有效輻射功率 (dBK) [B17:I17]
+		ERP [8]float64
+	}
+
+	FieldStrength struct {
+		// 54場強
+		FS54 struct {
+
+			// 預估54場強 (dB) [B20:I20]
+			F [8]float64
+
+			// 預估54場強傳送距離 (km) [B21:I21]
+			D [8]float64
+		}
+
+		// 60場強
+		FS60 struct {
+
+			// 預估60場強 (dB) [B25:I25]
+			F [8]float64
+
+			// 預估60場強傳送距離 (km) [B26:I26]
+			D [8]float64
+		}
+
+		// 80場強
+		FS80 struct {
+
+			// 預估80場強 (dB) [B30:I30]
+			F [8]float64
+
+			// 預估80場強傳送距離 (km) [B31:I31]
+			D [8]float64
+		}
+	}
+}
+
+func (rf *RFDATA) initData() {
+	fmt.Println("Init the RF data...")
+}
